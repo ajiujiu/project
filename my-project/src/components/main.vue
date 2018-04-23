221ce41f750409ee246d63812d2fa11a60bcbafb
小五 <wu.vkcyan@gmail.com>
2018-04-24 00:17:32 +0800

    构建基础框架 路由  主页面基础

---
+++ my-project/src/components/main.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="main">
+    <div class="video">
+      <iframe class="teaser-video" type="text/html" src="https://www.youtube.com/embed/-8BBKPrjb3A?autoplay=0&amp;origin=https://booc.io&amp;showinfo=0&amp;rel=0" frameborder="0">
+      </iframe>
+    </div>
+    <div class="main_text">
+      <p class="main_text_h1">Courses</p>
+      <div class="main_text_index">
+        <div>
+          dasdasd
+        </div>
+        <div>
+          dasdasd
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#main {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+}
+.video {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+.teaser-video {
+  width:600px;
+  height:338px;
+}
+.main_text {
+ 
+}
+.main_text_h1 {
+  font-size: 24px;
+}
+.main_text_index {
+ margin: 0 auto;
+  width: 1170px;
+  height: 50px;
+  border-radius: 4px;
+  border: 1px solid #f2f2f2; 
+}
+</style>
